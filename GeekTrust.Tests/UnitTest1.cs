701a5205892d2211a3c9318aac5bda861ebf22c2
Vaibhav Sharma <vaibhav.j.sharma@outlook.com>
2021-03-11 23:03:33 +0530

    Initial commit with console app template

---
+++ GeekTrust.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace GeekTrust.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ GeekTrust/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+namespace GeekTrust
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filename = args[0];
+            var fileStream = new FileStream(filename, FileMode.Open);
+            string line;
+
+            using (var reader = new StreamReader(fileStream))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    //TODO: Process input
+                }
+            }
+        }
+    }
+}
